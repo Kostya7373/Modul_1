fcffe1ee017432506a63576a39d4d8093fc4d56b
Kostya7373 <kot.gorobey@gmail.com>
2024-10-14 21:53:01 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,7 @@
+completed_DZ = 12
+hours_spent = 1.5
+Course_name = 'Python'
+
+print('В курсе ',Course_name,'выполнено ',completed_DZ,'заданий,', "затрачено времени ",hours_spent,'часов, ')
+print('среднее выполнение задания', hours_spent/completed_DZ, 'часов.')
+
---
+++ string methods.py
@@ -0,0 +1,10 @@
+my_string = input('Введите строку : ')
+print('1) Вы ввели строку (',my_string,')содержащую ', len(my_string),'символов')
+print('2) Верхний регистр ',my_string.upper())
+print('3) Нижний регистр ',my_string.lower())
+print('4) Удаление пробелов ', my_string.replace(' ',''))
+print('5) Первый символ строки ',my_string[0])
+print('6) Последний символ строки ',my_string[-1])
+
+
+
