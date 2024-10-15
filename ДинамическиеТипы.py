d40c5974c12db0778314f959cea04599d01dba6a
Kostya7373 <kot.gorobey@gmail.com>
2024-10-15 07:55:49 +0300

    Add files via upload

---
+++ ДинамическиеТипы.py
@@ -0,0 +1,13 @@
+variable= 'Константин'
+
+print('Мое имя',variable)
+
+age= 47
+
+print('Мой возраст',age, 'лет')
+
+print('в следующем году мне будет',age+1, 'лет')
+
+is_student = True
+
+print('Являюсь ли я студентом? ',is_student)
